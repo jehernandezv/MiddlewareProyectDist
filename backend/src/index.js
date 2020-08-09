fa2332f696634698e305b49ef5bb7e31056166ab
jehernandezv <jhonn.hernandez@uptc.edu.co>
2020-08-09 14:39:39 -0500

    cambio menor al middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,6 @@ var morgan = require('morgan');
 var body_parser = require('body-parser');
 var axios = require('axios');
 var port = process.argv[2];
-
 var app = express();
 
 app.use(cors());
@@ -12,15 +11,20 @@ app.use(morgan('dev'));
 app.use(body_parser.urlencoded({ extended: false }));
 
 app.get('/', async (req,res) => {
-    axios.get('http://localhost:5000/api').then(async (response) => {
+     sendRequestAxios(5000,'localhost',res);
+});
+    function sendRequestAxios(port,ip,res){
+    axios.get('http://'+ip+':'+port+'/api').then(async (response) => {
         await res.json({
             message:response.data
-        }).catch((error) => {
-            res.send(error);
-            console.log(error);
-        })
+        });
+    }).catch(async function (error) {
+      await res.json({
+           message:error.code
+       });
+      sendRequestAxios(6000,'localhost');
     });
-});
+}
 
 app.listen(port, function () {
     console.log('Example app server ubuntu1 listening on port ' + port + '!');
